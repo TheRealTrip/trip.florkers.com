baefc9c3fea25ac122d774e250c11cccc20ed1b3
TheRealTrip <oisin.oconnor101@gmail.com>
2022-11-13 00:00:22 +0000

    Added source code button

--- assets/js/index.js
+++ assets/js/index.js
@@ -2,6 +2,7 @@ const socials = {
     'github': "https://github.com/TheRealTrip",
     'noFriends': "https://www.youtube.com/watch?v=dQw4w9WgXcQ",
     'twitch': "https://www.twitch.tv/therealtrip0",
+    'source': "https://github.com/TheRealTrip/trip.florkers.com"
 }
 
 function openSocial(service) {
